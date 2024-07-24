594c5a45e337a5efc2379e826937b52b22d854c3
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-07-24 16:16:41 +0530

    done docker volume : '

--- python_project/myapp.py
+++ python_project/myapp.py
@@ -1,5 +1,19 @@
+import os
 
-print("Program to sum two numbers")
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-print("Sum of number are ", num1+num2)
+user_name = input("Enter your name to store in file or press Enter to proceed: ")
+if user_name:
+    # Ensure the directory exists
+    os.makedirs("python_project", exist_ok=True)
+    with open("python_project/user_info.txt", "a") as file:
+        file.write(user_name + "\n")
+
+show_info = input("Do you want to see all user names? y/n: ")
+if show_info.lower() == 'y':
+    try:
+        with open("python_project/user_info.txt", "r") as file:
+            content = file.readlines()
+    except Exception as e:
+        print(e, type(e))
+    else:
+        for line in content:
+            print(f"{line.rstrip()}")
